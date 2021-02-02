94c17fae05cebe45f582561b9aca80e601de6f75
KKe-coder <carumag726@gmail.com>
2021-02-02 07:19:23 +0000

    [Add] colorカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_01_030446) do
+ActiveRecord::Schema.define(version: 2021_02_02_071847) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_02_01_030446) do
     t.integer "list_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "color"
   end
 
 end
