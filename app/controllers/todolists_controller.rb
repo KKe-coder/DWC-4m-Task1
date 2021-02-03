b0d3450cc32c4a9583408eca361ed76409f4f04e
KKe-coder <carumag726@gmail.com>
2021-02-03 06:47:43 +0000

    [Add] 色計算式機能導入

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -7,13 +7,23 @@ class TodolistsController < ApplicationController
     list = List.new(list_params)
     list.score = Language.get_data(list_params[:body])
     list.save
-    #VisionAPIから返ってきたデータを配列化
-    tags =  Array.new([Vision.get_image_data(list.image)])
-    binding.pry
-    tags.each do |tag|
-      list.tags.create(name: tag.values.map{|i| i.to_s(16).rjust(2, '0') }.join.upcase)
-      #HTMLのカラーコードとして使用
-    end
+    dominant_color = Vision.get_image_data(list.image).values
+    list.tags.create(name: dominant_color.map{|i| i.to_s(16).rjust(2, '0') }.join.upcase)
+    #tagsには画像のドミナントカラーを代入する(HTMLカラーコードとして使用)
+    colors = { "red" => [220, 53, 69], "orange" => [255, 153, 51], "yellow" => [255, 193, 7], "green" => [40, 167, 69], "blue" => [0, 123, 255], "indigo" => [51, 51, 204], "purple" => [153, 51, 255]}
+    distance = {}
+    colors.each{|key, value|
+      r = "#{(value[0] - dominant_color[0])**2}"
+      g = "#{(value[1] - dominant_color[1])**2}"
+      b = "#{(value[2] - dominant_color[2])**2}"
+      distance[key] = "#{((r + g + b).to_i**(1 / 2.0)).round}"
+      #ここまででColor differenceのHashを作成
+      if key == "purple"
+        sorted_dis = distance.sort {|(k1, v1), (k2, v2)| v1.to_i <=> v2.to_i }.to_h
+        topcolor = sorted_dis.first.first
+        list.tags.update(color: topcolor)
+      end
+    }
     redirect_to todolist_path(list.id)
   end
 
